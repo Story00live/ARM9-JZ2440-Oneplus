518599be95d4e09db39fe670eef888a72b7a1dce
KangYunjie <Story00live@users.noreply.github.com>
2018-08-24 22:16:30 +0800

    2018-08-24 未定义指令执行异常跳转

--- 014_und_exception/001/exception.c
+++ 014_und_exception/001/exception.c
@@ -1,61 +1,33 @@
 /**
   *********************************************************************************************************
-  * @File               : main.c 
+  * @File               : exception.c 
   * @Author             : kang.yunjie 
   * @version            : V1.0 
-  * @Date               : 2018-08-03 22:05:27 
-  * @Brief              : Main
+  * @Date               : 2018-08-24 21:07:37 
+  * @Brief              : 
   *********************************************************************************************************
   * @attention          : 
   *                       
   *********************************************************************************************************
   */
 
-#include "s3c2440_soc.h"
-#include "led.h"
+#include "exception.h"
 #include "uart.h"
-#include "my_printf.h"
-#include "init.h"
-
-char g_Char = 'A';
-char g_Char3 = 'a';
-int g_Char5 = 60000;
-const char g_Char2 = 'B';
-const int g_Char4 = 10000;
-int g_A = 0;
-int g_B;
 
 /**********************************************************************************************************
- @Function               int main(void)
- @Description            main
- @Input                  void
+ @Function               void printException(unsigned int cpsr, char *str)
+ @Description            printException
+ @Input                  unsignde int
+					char*
  @Return                 void
 **********************************************************************************************************/
-int main(void)
+void printException(unsigned int cpsr, char* str)
 {
-	puts("\r\ng_A = ");
-	printHex(g_A);
+	puts("Exception! cpsr = ");
+	printHex(cpsr);
+	puts(" ");
+	puts(str);
 	puts("\r\n");
-	
-	puts("\r\ng_A = ");
-	printHex(g_Char2);
-	puts("\r\n");
-	
-	puts("\r\ng_A = ");
-	printHex(g_Char4);
-	puts("\r\n");
-	
-	while (true) {
-		putchar(g_Char);
-		g_Char++;
-		
-		putchar(g_Char3);
-		g_Char3++;
-		
-		delay(1000000);
-	}
-	
-	return 0;
 }
 
 /********************************************** END OF FLEE **********************************************/
---
+++ 014_und_exception/002/exception.h
@@ -0,0 +1,8 @@
+#ifndef __JZ2440_EXCEPTION_H
+#define   __JZ2440_EXCEPTION_H
+
+#include "s3c2440_soc.h"
+
+void printException(unsigned int cpsr, char* str);
+
+#endif
