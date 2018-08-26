b1fe51365d00d415c7bf5734a3cf39ca0ed245a6
KangYunjie <Story00live@users.noreply.github.com>
2018-08-26 20:34:17 +0800

    2018-08-26 软件中断

---
+++ 015_swi_exception/001/uart.h
@@ -0,0 +1,16 @@
+#ifndef __JZ2440_UART_H
+#define   __JZ2440_UART_H
+
+#include "s3c2440_soc.h"
+
+void uart0_init(void);
+
+int putchar(int cByte);
+int getchar(void);
+int puts(const char *pString);
+int printHex(unsigned int val);
+
+void dbg_print1(void);
+void dbg_print2(void);
+
+#endif
